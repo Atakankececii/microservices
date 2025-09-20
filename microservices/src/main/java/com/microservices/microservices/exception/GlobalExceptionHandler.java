9a26db4404086db751642e43ca1449ee5cb13b60
Atakankececii <atakankececicom@gmail.com>
2025-09-20 13:10:38 +0300

    Handle all types of runtime exceptions

--- microservices/src/main/java/com/microservices/microservices/exception/GlobalExceptionHandler.java
+++ microservices/src/main/java/com/microservices/microservices/exception/GlobalExceptionHandler.java
@@ -13,6 +13,17 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception ex, WebRequest request){
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                request.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 
     @ExceptionHandler(CustomerAlreadyExistsException.class)
     public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsException ex, WebRequest request){
