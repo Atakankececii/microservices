0521bf0be6c395cbf1176997e096cb021b6fcf24
Atakankececii <atakankececicom@gmail.com>
2025-09-20 13:22:09 +0300

    Test configurations

---
+++ microservices/src/test/java/com/microservices/microservices/controller/AccountControllerTest.java
@@ -0,0 +1,10 @@
+package com.microservices.microservices.controller;
+
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(AccountsController.class)
+public class AccountControllerTest {
+
+
+}
